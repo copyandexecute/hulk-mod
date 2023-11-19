1274fc37bc733f2838e5e08733246aa3dee8acbc
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2023-11-19 13:22:59 +0100

    added landing

--- src/main/kotlin/gg/norisk/hulk/client/abilities/JumpManager.kt
+++ src/main/kotlin/gg/norisk/hulk/client/abilities/JumpManager.kt
@@ -6,15 +6,23 @@ import gg.norisk.hulk.common.network.NetworkManager
 import gg.norisk.hulk.common.registry.SoundRegistry
 import gg.norisk.hulk.common.utils.CameraShaker
 import gg.norisk.hulk.common.utils.HulkUtils
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.network.ClientPlayerEntity
+import net.minecraft.client.sound.MovingSoundInstance
 import net.minecraft.client.sound.PositionedSoundInstance
-import net.minecraft.client.sound.SoundManager
+import net.minecraft.client.sound.SoundInstance
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
+import net.minecraft.util.math.MathHelper
 import net.silkmc.silk.core.annotations.ExperimentalSilkApi
 import net.silkmc.silk.core.entity.posUnder
 import kotlin.math.max
@@ -26,6 +34,7 @@ object JumpManager : ClientTickEvents.EndTick, HudRenderCallback {
     private val jumpKey by lazy { MinecraftClient.getInstance().options.jumpKey }
     private var isCharging = false
     private var jumpStrength = 0.0
+    private var isFlying = false
     private const val MAX_JUMP_STRENGTH = 10.0
 
     @OptIn(ExperimentalSilkApi::class)
@@ -41,6 +50,7 @@ object JumpManager : ClientTickEvents.EndTick, HudRenderCallback {
             isCharging = true
         } else if (isJumpKey && event.action == 0 && isCharging) {
             isCharging = false
+            isFlying = true
             NetworkManager.jumpPacket.send(jumpStrength)
             handleCracks()
             jumpStrength = 0.0
@@ -50,7 +60,7 @@ object JumpManager : ClientTickEvents.EndTick, HudRenderCallback {
     private fun handleCracks() {
         val player = MinecraftClient.getInstance().player ?: return
 
-        CameraShaker.addEvent(CameraShaker.BoomShake(0.1* jumpStrength, 0.1, 0.3))
+        CameraShaker.addEvent(CameraShaker.BoomShake(0.1 * jumpStrength, 0.1, 0.3))
 
         val startPos = player.posUnder
         MinecraftClient.getInstance().soundManager.play(
@@ -61,6 +71,10 @@ object JumpManager : ClientTickEvents.EndTick, HudRenderCallback {
             )
         )
         val radius = jumpStrength.toInt() * 4
+        createCracks(player, startPos, radius)
+    }
+
+    private fun createCracks(player: PlayerEntity, startPos: BlockPos, radius: Int) {
         for (blockPos in HulkUtils.generateSphere(startPos, radius, false)) {
             val state = player.world.getBlockState(blockPos)
             if (state.isAir) continue
@@ -77,14 +91,16 @@ object JumpManager : ClientTickEvents.EndTick, HudRenderCallback {
             val randomBonus = Random.nextInt(-3, 4)
             progressStrength = min(9, max(-1, progressStrength + randomBonus))
 
-            player.world.playSoundAtBlockCenter(
-                blockPos,
-                state.soundGroup.breakSound,
-                SoundCategory.BLOCKS,
-                0.4f,
-                Random.nextDouble(0.8, 1.0).toFloat(),
-                true
-            )
+            if (Random.nextBoolean()) {
+                player.world.playSoundAtBlockCenter(
+                    blockPos,
+                    state.soundGroup.breakSound,
+                    SoundCategory.BLOCKS,
+                    0.4f,
+                    Random.nextDouble(0.8, 1.0).toFloat(),
+                    true
+                )
+            }
             //player.world.addBlockBreakParticles(blockPos,state)
             player.world.setBlockBreakingInfo(Random.nextInt(), blockPos, progressStrength)
         }
@@ -94,6 +110,28 @@ object JumpManager : ClientTickEvents.EndTick, HudRenderCallback {
         if (isCharging) {
             jumpStrength = min(jumpStrength + 0.2, MAX_JUMP_STRENGTH)
         }
+        val player = client.player ?: return
+        if (isFlying) {
+            if (player.isOnGround) {
+                isFlying = false
+                handleLanding()
+            }
+        }
+    }
+
+    private fun handleLanding() {
+        CameraShaker.addEvent(CameraShaker.BoomShake(Random.nextDouble(0.3, 0.5), 0.1, 0.3))
+
+        MinecraftClient.getInstance().soundManager.play(
+            PositionedSoundInstance.master(
+                SoundRegistry.JUMP,
+                1.0f,
+                Random.nextDouble(0.5, 1.3).toFloat()
+            )
+        )
+
+        val player = MinecraftClient.getInstance().player ?: return
+        createCracks(player, player.posUnder, Random.nextInt(6, 14))
     }
 
     override fun onHudRender(drawContext: DrawContext, tickDelta: Float) {
